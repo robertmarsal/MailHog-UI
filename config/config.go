68411564e0f1f8b4b622f2c8026a5b517ae3c7c6
Robert Marsal <robertboloc@gmail.com>
2020-12-14 18:11:26 +0000

    Add dark mode support

--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ func DefaultConfig() *Config {
 	return &Config{
 		APIHost:    "",
 		UIBindAddr: "0.0.0.0:8025",
+		DarkMode:   false,
 		WebPath:    "",
 	}
 }
@@ -17,6 +18,7 @@ func DefaultConfig() *Config {
 type Config struct {
 	APIHost    string
 	UIBindAddr string
+	DarkMode   bool
 	WebPath    string
 }
 
@@ -29,4 +31,5 @@ func Configure() *Config {
 func RegisterFlags() {
 	flag.StringVar(&cfg.APIHost, "api-host", envconf.FromEnvP("MH_API_HOST", "").(string), "API URL for MailHog UI to connect to, e.g. http://some.host:1234")
 	flag.StringVar(&cfg.UIBindAddr, "ui-bind-addr", envconf.FromEnvP("MH_UI_BIND_ADDR", "0.0.0.0:8025").(string), "HTTP bind interface and port for UI, e.g. 0.0.0.0:8025 or just :8025")
+	flag.BoolVar(&cfg.DarkMode, "dark-mode", envconf.FromEnvP("MH_DARK_MODE", false).(bool), "Enable dark mode")
 }
